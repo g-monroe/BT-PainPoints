8a2119ecbc8d4bb3fa5a8577966f943b1e71bc86
Dallas Evers <dallas.evers@buildertrend.com>
2019-05-29 16:07:13 -0500

    Custom drop down header. Doesn't work

--- BTSuggestions/BTSuggestions.Web/ClientApp/src/App.tsx
+++ BTSuggestions/BTSuggestions.Web/ClientApp/src/App.tsx
@@ -4,7 +4,8 @@ import "antd/dist/antd.css";
 import { Link, BrowserRouter, Route } from 'react-router-dom';
 import CreateForm from '../src/components/CreateForm';
 import blankTemplate from '../src/types/blankTemplate.api.json';
-import PainPointPage from '../src/components/PainPointPage';
+import CustomColumnHeader from './components/CustomColumnHeader';
+import {ColumnNameList} from '../src/types/dropdownValues/ColumnNameTypes'
 
 export default class App extends React.Component {
     displayName = App.name
@@ -20,7 +21,7 @@ export default class App extends React.Component {
                         <Link to="/create" className="navLinks" style = {{padding:15,margin:15}}>Create New Issue</Link>
                         <Link to="/admin" className="navLinks" style = {{padding:15,margin:15}}>Manage Issues</Link>
                     </nav>
-                    <Route path="/home" exact render={(props) => <PainPointPage/>}></Route>
+                    <Route path="/home" exact render={(props) => <CustomColumnHeader menuList={ColumnNameList}/>}></Route>
                     <Route path="/create" exact render={(props) => <CreateForm data={blankTemplate}/>}></Route>
                     <Route path="/admin"></Route>
                 </BrowserRouter>
---
+++ BTSuggestions/BTSuggestions.Web/ClientApp/src/types/dropdownValues/ColumnNameTypes.ts
@@ -0,0 +1,10 @@
+import {SelectOption} from './SelectOption';
+
+export const ColumnNameList: SelectOption[] =[
+    {id: 1, name: "Pain Point Type"},
+    {id: 2, name: "Pain Point Severity"},
+    {id: 3, name: "Description"},
+    {id: 4, name: "Highest Keyword"},
+    {id: 5, name: "Date Created"},
+    {id: 6, name: "Status"}
+]
