387a27d0e608948b6395224be0145c2db85b9e65
jamesfox16 <james.fox@buildertrend.com>
2019-05-29 11:57:50 -0500

    Fixed issue with AddTransient

--- BTSuggestions/BTSuggestions/Startup.cs
+++ BTSuggestions/BTSuggestions/Startup.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BTSuggestions.Core.Interfaces.DataAccessHandlers;
+using BTSuggestions.Core.Interfaces.Engines;
+using BTSuggestions.Core.Interfaces.Managers;
+using BTSuggestions.DataAccessHandlers;
+using BTSuggestions.Engines;
+using BTSuggestions.Managers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -44,6 +50,19 @@ namespace BTSuggestions
                 options.AddPolicy("MyPolicy",
                                   builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             });
+
+            // Managers
+            services.AddTransient<ICommentManager, CommentManager>();
+            services.AddTransient<IPainPointManager, PainPointManager>();
+            services.AddTransient<IUserManager, UserManager>();
+            // Engines
+            services.AddTransient<ICommentEngine, CommentEngine>();
+            services.AddTransient<IPainPointEngine, PainPointEngine>();
+            services.AddTransient<IUserEngine, UserEngine>();
+            // Handlers
+            services.AddTransient<ICommentHandler, CommentHandler>();
+            services.AddTransient<IPainPointHandler, PainPointHandler>();
+            services.AddTransient<IUserManager, UserManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
