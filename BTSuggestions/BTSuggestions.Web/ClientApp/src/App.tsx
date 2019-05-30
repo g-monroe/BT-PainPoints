489591ab3c450a215ef4ae9f291c6044a27c444e
Dallas Evers <dallas.evers@buildertrend.com>
2019-05-30 16:20:56 -0500

    Added columns with custom headers

--- BTSuggestions/BTSuggestions.Web/ClientApp/src/App.tsx
+++ BTSuggestions/BTSuggestions.Web/ClientApp/src/App.tsx
@@ -6,6 +6,8 @@ import CreateForm from '../src/components/CreateForm';
 import DetailView from '../src/components/DetailView';
 import blankTemplate from '../src/types/blankTemplate.api.json';
 import testData from '../src/types/testData.api.json';
+import CustomColumns from '../src/components/CustomColumns'
+
 
 
 export default class App extends React.Component {
@@ -15,6 +17,7 @@ export default class App extends React.Component {
         const css = "../src/App.css";
         return (
             <>
+            
                 <style>{css}</style>
                 <BrowserRouter>
                     <nav>
@@ -22,10 +25,12 @@ export default class App extends React.Component {
                         <Link to="/create" className="navLinks" style = {{padding:15,margin:15}}>Create New Issue</Link>
                         <Link to="/admin" className="navLinks" style = {{padding:15,margin:15}}>Manage Issues</Link>
                     </nav>
+                    <Route path="/home" exact render={(props) => <CustomColumns/>}/>
                     <Route path="/create" exact render={(props) => <CreateForm data={blankTemplate} />}/>
                     <Route path="/home/:id" exact render={(props) => <DetailView data={testData}/>}/>
                     <Route path="/admin"></Route>
                 </BrowserRouter>
+                
             </>
         );
     }
--- BTSuggestions/BTSuggestions.Web/ClientApp/src/types/dropdownValues/ColumnNameTypes.ts
+++ BTSuggestions/BTSuggestions.Web/ClientApp/src/types/dropdownValues/ColumnNameTypes.ts
@@ -1,10 +1,16 @@
 import {SelectOption} from './SelectOption';
 
-export const ColumnNameList: SelectOption[] =[
-    {id: 1, name: "Pain Point Type"},
-    {id: 2, name: "Pain Point Severity"},
-    {id: 3, name: "Description"},
-    {id: 4, name: "Highest Keyword"},
-    {id: 5, name: "Date Created"},
-    {id: 6, name: "Status"}
+export class SelectOptionWithEntity implements SelectOption {
+    id: number;
+    entityName: string;
+    name: string;
+}
+
+export const columnNameList: SelectOptionWithEntity[] =[
+    {id: 1, entityName:"painPointType" ,name: "Pain Point Type"},
+    {id: 2, entityName:"painPointSeverity" ,name: "Pain Point Severity"},
+    {id: 3, entityName:"description" ,name: "Description"},
+    {id: 4, entityName:"highestKeyword" ,name: "Highest Keyword"},
+    {id: 5, entityName:"dateCreated" ,name: "Date Created"},
+    {id: 6, entityName:"status" ,name: "Status"}
 ]
