1bb4b3633da0d0d2c8dc17691046de360a9bdea9
jamesfox16 <james.fox@buildertrend.com>
2019-06-03 16:36:00 -0500

    added more to create account

--- BTSuggestions/BTSuggestions.Web/ClientApp/src/components/CreateAccount.tsx
+++ BTSuggestions/BTSuggestions.Web/ClientApp/src/components/CreateAccount.tsx
@@ -7,23 +7,67 @@ interface ICreateAccountProps {
 
 interface ICreateAccountState {
     email: string,
+    emailConfirm: string,
     firstName: string,
     lastName: string,
     username: string,
-    password: string
+    password: string,
+    passwordConfirm: string
 }
 
 export default class CreateAccount extends React.Component {
     
+    handleEmailChange = (event: any) => {
+        this.setState({
+            email: event.target.value
+        });
+    }
+
+    handleConfirmEmailChange = (event: any) => {
+        this.setState({
+            emailConfirm: event.target.value
+        })
+    }
+
+    handleFirstNameChange = (event: any) => {
+        this.setState({
+            firstName: event.target.value
+        });
+    }
+
+    handleLastNameChange = (event: any) => {
+        this.setState({
+            lastName: event.target.value
+        });
+    }
+
+    handleUsernameChange = (event: any) => {
+        this.setState({
+            username: event.target.value
+        });
+    }
+
+    handlePasswordChange = (event: any) => {
+        this.setState({
+            password: event.target.value
+        });
+    }
+
+    handlePasswordConfirmChange = (event: any) => {
+        this.setState({
+            passwordConfirm: event.target.value
+        });
+    }
     
     render() {
         return <>
-            <Input placeholder='Email'/>
-            <Input placeholder='First Name'/>
-            <Input placeholder='Last Name'/>
-            <Input placeholder='Username'/>
-            <Input placeholder='Password'/>
-            <Input placeholder='Confirm Password' />
+            <Input placeholder='Email' onChange={this.handleEmailChange} />
+            <Input placeholder='Confirm Email' onChange={this.handleConfirmEmailChange} />
+            <Input placeholder='First Name' onChange={this.handleFirstNameChange} />
+            <Input placeholder='Last Name' onChange={this.handleLastNameChange} />
+            <Input placeholder='Username' onChange={this.handleUsernameChange} />
+            <Input placeholder='Password' onChange={this.handlePasswordChange} />
+            <Input placeholder='Confirm Password' onChange={this.handlePasswordConfirmChange} />
             <Button type='primary'>Create Account</Button>
         </>
     }
