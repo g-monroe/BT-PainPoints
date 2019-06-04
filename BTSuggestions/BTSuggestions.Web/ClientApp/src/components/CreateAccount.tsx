a6c69687ee74255ac6dc5846fe61acb02b471e9e
jamesfox16 <james.fox@buildertrend.com>
2019-06-04 11:24:35 -0500

    Added to user info and entity

--- BTSuggestions/BTSuggestions.Web/ClientApp/src/components/CreateAccount.tsx
+++ BTSuggestions/BTSuggestions.Web/ClientApp/src/components/CreateAccount.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Input, Button, message } from 'antd';
+import { Input, Button, message, Form } from 'antd';
 import * as yup from 'yup';
+import FormItem from 'antd/lib/form/FormItem';
+import { withFormik, validateYupSchema } from 'formik';
+import UserEntity from '../entity/UserEntity';
 
 interface ICreateAccountProps {
     newUser: (username: string) => void
+    data: UserEntity,
+    emailConfirmProp?: string,
+    passwordConfirmProp?: string;
 }
 
 interface ICreateAccountState {
@@ -18,16 +24,16 @@ interface ICreateAccountState {
 
 //TODO: work on fixing this to make sure that it lines up with what we want.
 const yupValidation = yup.object().shape<ICreateAccountState>({
-    email: yup.string().required(),
-    emailConfirm: yup.string().matches(this.state.email).required(),
-    firstName: yup.string().required(),
-    lastName: yup.string().required(),
-    username: yup.string().max(30).required(),
-    password: yup.string().min(10).required(),
-    passwordConfirm: yup.string().min(10).required()
+    email: yup.string().required().label('Email'),
+    emailConfirm: yup.string().required().label('Email Confirm'),
+    firstName: yup.string().required().label("First Name"),
+    lastName: yup.string().required().label('Last Name'),
+    username: yup.string().max(30).required().label('Username'),
+    password: yup.string().min(10).required().label('Password'),
+    passwordConfirm: yup.string().min(10).required().label('Password Confirm')
 })
 
-export default class CreateAccount extends React.Component {
+class CreateAccount extends React.Component {
     // Default state so that the verification fucntion will work correctly.
     state = {
         email: "",
@@ -37,6 +43,10 @@ export default class CreateAccount extends React.Component {
         username: "",
         password: "",
         passwordConfirm: ""
+    }
+
+    validateEmailChange = (event: any) => {
+
     }
     
     // Handlers for the state changes
@@ -107,15 +117,48 @@ export default class CreateAccount extends React.Component {
     
     render() {
         return <>
-            <Input placeholder='Email' onChange={this.handleEmailChange} />
-            <Input placeholder='Confirm Email' onChange={this.handleConfirmEmailChange} />
-            <Input placeholder='First Name' onChange={this.handleFirstNameChange} />
-            <Input placeholder='Last Name' onChange={this.handleLastNameChange} />
-            <Input placeholder='Username' onChange={this.handleUsernameChange} />
-            <Input placeholder='Password' onChange={this.handlePasswordChange} />
-            <Input placeholder='Confirm Password' onChange={this.handlePasswordConfirmChange} />
-            <Button type='primary' onClick={this.handleCreateAccountClick}>Create Account</Button>
-            <Button type='danger' onClick={this.handleCancleCreateClick}>Cancle Create</Button>
+            <Form  >
+                <FormItem label='Email' required >
+                    <Input id='Email' placeholder='Email' onChange={this.handleEmailChange} />
+                </FormItem>
+                <FormItem label='Confirm Email' required>
+                    <Input id='EmailConfirm' placeholder='Confirm Email' onChange={this.handleConfirmEmailChange} />
+                </FormItem>
+                <FormItem label='First Name' required>
+                    <Input id='firstName' placeholder='First Name' onChange={this.handleFirstNameChange} />
+                </FormItem>
+                <FormItem id='lastName' label='Last Name' required>
+                    <Input placeholder='Last Name' onChange={this.handleLastNameChange} />
+                </FormItem>
+                <FormItem id='username' label='Username' required>
+                    <Input placeholder='Username' onChange={this.handleUsernameChange} />
+                </FormItem>
+                <FormItem id='password' label='Password' required>
+                    <Input placeholder='Password' onChange={this.handlePasswordChange} />
+                </FormItem>
+                <FormItem id='passwordConfirm' label='Confirm Password' required>
+                    <Input placeholder='Confirm Password' onChange={this.handlePasswordConfirmChange} />
+                </FormItem>
+                <Button id='createButton' type='primary' onClick={this.handleCreateAccountClick}>Create Account</Button>
+                <Button id='cancleButton' type='danger' onClick={this.handleCancleCreateClick}>Cancle Create</Button>
+            </Form>
         </>
     }
-}
+};
+
+export default withFormik<ICreateAccountProps, ICreateAccountState>({
+    mapPropsToValues: props => ({
+        email: props.data.email,
+        emailConfirm: props.emailConfirmProp,
+        firstName: props.data.firstName,
+        lastName: props.data.lastName,
+        username: props.data.username,
+        password: props.data.username,
+        passwordConfirm: props.passwordConfirmProp
+    }),
+    validationSchema: yupValidation,
+    handleSubmit: (values) => {
+        console.log(values + 'User created');
+    },
+    displayName: 'Create Account'
+})(CreateAccount);
