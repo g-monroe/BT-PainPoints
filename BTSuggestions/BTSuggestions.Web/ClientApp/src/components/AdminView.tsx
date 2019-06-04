731698b3391e75a4c6696193501e2f759f853aa0
makriege <madison.kriege@buildertrend.com>
2019-06-04 12:21:32 -0500

    Overhaul of AdminView, new custom table

--- BTSuggestions/BTSuggestions.Web/ClientApp/src/components/AdminView.tsx
+++ BTSuggestions/BTSuggestions.Web/ClientApp/src/components/AdminView.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import '../styles/App.css';
-import { Layout, Button, Table, Icon, Select, Input, InputNumber} from 'antd';
+import { Layout, Button, Icon, Select, Input, InputNumber, Checkbox} from 'antd';
 import AdminViewEntity from '../entity/AdminViewEntity';
 import { statusList } from '../types/dropdownValues/statusTypes';
 import { painPointList } from '../types/dropdownValues/painPointTypes';
 import { industryList } from '../types/dropdownValues/industryTypes';
+import { SelectOption } from '../types/dropdownValues/SelectOption';
+import '../styles/AdminView.css';
 
 const { Content} = Layout;
 
@@ -24,88 +26,54 @@ export default class AdminView extends React.Component<IAdminViewProps, IAdminVi
     state = {
         issues: this.props.data.issues
     }
-
-    columns = [
-        {
-            title: 'Issue Title',
-            dataIndex: 'painPointTitle',
-            fixed: true,
-            render: (text: string) => <a href="/home/:id">{text}</a>
-        }, 
-        {
-            title: 'ID',
-            width: 50,
-            dataIndex: 'painPointId'
-        },
-        {
-            title: 'Description',
-            width: 250,
-            dataIndex: 'painPointSummary',
-            render: (text: string) => <Input value={text}/>
-        },
-        {
-            title: 'Annotation',
-            width: 250,
-            dataIndex: 'painPointAnnotation',
-            render: (text: string) => <Input value={text}/>
-        },
-        {
-            title: 'Issue Type',
-            dataIndex: 'painPointType',
-            width: 100,
-            render: (text : string) => <Select defaultValue={text}>{painPointList.map((s:any) => <Select.Option key={s.id} value={s.id}>{s.name}</Select.Option>)}</Select>
-        },
-        {
-            title: 'Severity',
-            dataIndex: 'painPointSeverity',
-            width: 15,
-            render: (text: number) => <InputNumber value={text}/>
-        },
-        {
-            title: 'Company Name',
-            dataIndex: 'companyName',
-            width: 150,
-            render: (text: string) => <Input value={text}/>
-        },
-        {
-            title: 'Industry Type',
-            dataIndex: 'industryType',
-            render: (text: string) => <Select defaultValue={text}>{industryList.map((s:any) => <Select.Option key={s.id} value={s.id}>{s.name}</Select.Option>)}</Select>
-        },
-        {
-            title: 'Date Posted',
-            dataIndex: 'datetime'
-        },
-        {
-            title: 'Issue Status',
-            dataIndex: 'submissionStatus',
-            width: 200,
-            render: (text: string) => <Select defaultValue={text}>{statusList.map((s:any) => <Select.Option key={s.id} value={s.id}>{s.name}</Select.Option>)}</Select>
-        },
-        {
-            title: 'Edit',
-            dataIndex: 'editIndex',
-            width: 25,
-            render: (index: number) => <Button onClick={()=>alert("Clicked!")}><Icon type="edit"></Icon></Button>
-        },
-    ]
-
-    rowSelection = {
-        getCheckboxProps: (record: string) => ({
-            name: record
-        })
+    
+    renderDropdowns = (list: SelectOption[]) => {
+        return list.map((value:any) => (
+            <Select.Option key={value.id} value={value.id}>{value.name}</Select.Option>))
     };
 
+    handleChange = (e:any) => {
+        console.log(e, e.target, e.target.value);
+        this.setState(
+            { issues: e.target.value}
+        )
+    }
+
     render() {
-        const css = "../src/styles/App.css";
         return (
             <Layout>
-                <style>
-                    {css}
-                </style>
                 <Content>
-                    <h2>There should probably be a title here</h2>
-                    <Table rowSelection={this.rowSelection} columns={this.columns} dataSource={this.state.issues} scroll={{ x: 1300 }}/>
+                    <h2>Admin Console</h2>
+                    <table className="adminTable">
+                        <thead className="tableHeader"><tr>
+                            <td></td>
+                            <td className="thickColumn">Issue Title</td>
+                            <td className="thickColumn">Description</td>
+                            <td className="thickColumn">Annotation</td>
+                            <td className="thickColumn">Issue Type</td>
+                            <td className="thinColumn">Severity</td>
+                            <td className="medColumn">Company Name</td>
+                            <td className="medColumn">Industry Type</td>
+                            <td className="medColumn">Date Posted</td>
+                            <td className="medColumn">Submission Status</td>
+                            <td className="thinColumn">Save Changes</td>
+                        </tr></thead>
+                        <tbody>
+                            {this.state.issues.map(i => (<tr key={i.painPointId}>
+                                <td><Checkbox/></td>
+                                <td className="thickColumn"><a href={"/home/" + i.painPointId}>{i.painPointTitle}</a></td>
+                                <td className="thickColumn"><Input key={"summary"} value={i.painPointSummary} onChange={this.handleChange} /></td>
+                                <td className="thickColumn"><Input key={"annotation"} value={i.painPointAnnotation}/></td>
+                                <td className="thickColumn"><Select key={"type"} defaultValue={i.painPointType}>{this.renderDropdowns(painPointList)}</Select></td>
+                                <td className="thinColumn"><InputNumber key={"severity"} value={i.painPointSeverity}/></td>
+                                <td className="medColumn"><Input key={"company"} value={i.companyName}/></td>
+                                <td className="medColumn"><Select key={"industry"} defaultValue={i.industryType}>{this.renderDropdowns(industryList)}</Select></td>
+                                <td className="medColumn">{i.datetime}</td>
+                                <td className="medColumn"><Select key={"status"} defaultValue={i.submissionStatus}>{this.renderDropdowns(statusList)}</Select></td>
+                                <td className="thinColumn"><Button onClick={()=>alert("Clicked!")}><Icon type="save"></Icon></Button></td>
+                            </tr>))}
+                        </tbody>
+                    </table>
                     <Button>Create Group</Button>
                     <Button>Delete</Button>
                 </Content>
