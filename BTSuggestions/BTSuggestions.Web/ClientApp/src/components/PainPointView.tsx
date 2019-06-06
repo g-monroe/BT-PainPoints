f2918a2a28448997f520d14d91ed5555f14bceb3
Dallas Evers <dallas.evers@buildertrend.com>
2019-06-06 12:19:03 -0500

    quick changes

--- BTSuggestions/BTSuggestions.Web/ClientApp/src/components/PainPointView.tsx
+++ BTSuggestions/BTSuggestions.Web/ClientApp/src/components/PainPointView.tsx
@@ -23,13 +23,13 @@ export default class PainPointView extends React.Component<IPainPointViewProps,I
 
   render () {
 
-    return ( <><Collapse style={{marginLeft:'10%', marginRight:'10%', marginBottom: 16, marginTop: 16}} defaultActiveKey={['1','2']} >
+    return ( <><Collapse style={{marginLeft:'10%', marginRight:'10%', marginBottom: 16, marginTop: 16}} defaultActiveKey={['1']} >
     <Panel header="To Do" key="1">
       <PainPointTable/>
     </Panel>      
   </Collapse>
-  <Collapse style={{margin:15}} defaultActiveKey={['1','2']} >
-    <Panel header="Open Pain Points" key="1">
+  <Collapse style={{margin:15}} defaultActiveKey={['2']} >
+    <Panel header="Open Pain Points" key="2">
       <PainPointTable/>
     </Panel>      
   </Collapse>
