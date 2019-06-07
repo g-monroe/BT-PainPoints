bc3354636764a95698a02441c13f65cef318b8ef
makriege <madison.kriege@buildertrend.com>
2019-06-07 15:42:17 -0500

    Fixed grid, sliding inputs remain

--- BTSuggestions/BTSuggestions.Web/ClientApp/src/components/CreateForm.tsx
+++ BTSuggestions/BTSuggestions.Web/ClientApp/src/components/CreateForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Form as AntForm, Select, Input, Button, Slider, Icon, message } from 'antd';
+import { Layout, Form as AntForm, Select, Input, Button, Slider, Icon, message, Card } from 'antd';
 import * as yup from 'yup';
 import { painPointList } from '../types/dropdownValues/painPointTypes';
 import { industryList } from '../types/dropdownValues/industryTypes';
@@ -19,20 +19,20 @@ interface ICreateFormProps {
 }
 
 interface ICreateFormState {
-        title: string;
-        summary: string;
-        annotation?: string;
-        status?: string;
-        userId?: number;
-        priorityLevel: number;
-        companyName?: string;
-        companyContact?: string;
-        companyLocation?: string;
-        industryType?: string;
-        createdOn?: Date;
-        types: string[];
+    title: string;
+    summary: string;
+    annotation?: string;
+    status?: string;
+    userId?: number;
+    priorityLevel: number;
+    companyName?: string;
+    companyContact?: string;
+    companyLocation?: string;
+    industryType?: string;
+    createdOn?: Date;
+    types: string[];
 }
-interface INewCreateForm{
+interface INewCreateForm {
     painPointType: string[],
     painPointTitle: string,
     painPointSummary: string,
@@ -61,7 +61,7 @@ class CreateForm extends React.Component<InjectedFormikProps<ICreateFormProps, I
     static defaultProps = {
         painPointHandler: new PainPointHandler()
     };
-    
+
     state = {
         inputValue: 0
     };
@@ -72,8 +72,8 @@ class CreateForm extends React.Component<InjectedFormikProps<ICreateFormProps, I
         });
     };
     handleSave = async (entity: PainPointEntity): Promise<void> => {
-          await this.props.painPointHandler!.createPainPoint(entity)
-      }
+        await this.props.painPointHandler!.createPainPoint(entity)
+    }
     getValidationStatus = (error: any) => {
         return !!error ? 'error' : 'success';
     };
@@ -93,51 +93,52 @@ class CreateForm extends React.Component<InjectedFormikProps<ICreateFormProps, I
         return (
             <Layout>
                 <Content>
-                    <Form onSubmitCapture={handleSubmit} className="create-form">
-                        <h1>Create New Issue</h1>
-                        <div className="required-components">
-                            <FormItem className="input" label="Issue Title" required validateStatus={this.getValidationStatus(errors.title)}>
-                                <Input id="title" placeholder="Title" value={values.title} onChange={handleChange} />
-                            </FormItem>
-                            <FormItem className="input" label="Issue Summary" required validateStatus={this.getValidationStatus(errors.summary)}>
-                                <TextArea id="summary" placeholder="Description of Problem" value={values.summary} onChange={handleChange} rows={2} />
-                            </FormItem>
-                            <FormItem className="input" label="Issue Annotation" validateStatus={this.getValidationStatus(errors.annotation)}>
-                                <TextArea id="annotation" placeholder="Personal Notes about Problem" value={values.annotation} onChange={handleChange} rows={2} />
-                            </FormItem>
-                            <FormItem className="input" label="Issue Type" required validateStatus={this.getValidationStatus(errors.types)}>
-                                <Select mode="multiple" id="types" onChange={(x:any) => setFieldValue("types", x)} value={values.types}>{this.renderDropdowns(painPointList)}</Select>
-                            </FormItem>
-                            <FormItem label="Issue Severity" required validateStatus={this.getValidationStatus(errors.priorityLevel)}>
-                                <div className="wrapper">
-                                    <Icon type="fire" theme="twoTone" twoToneColor="#ff3300" />
-                                    <Slider tipFormatter={this.sliderFormatter} className="slider" id="painPointSeveritySlide" min={0} max={100} onChange={this.slideChange} value={typeof inputValue === 'number' ? inputValue : 0} />
-                                    <Icon type="fire" theme="twoTone" twoToneColor="#ff3300" />
-                                    <Icon type="fire" theme="twoTone" twoToneColor="#ff3300" />
-                                    <Icon type="fire" theme="twoTone" twoToneColor="#ff3300" />
+                    <Card title="Create New Issue">
+                        <Card type="inner">
+                            <Form onSubmitCapture={handleSubmit} className="create-form">
+                                <div className="required-components">
+                                    <FormItem className="input" label="Issue Title" required validateStatus={this.getValidationStatus(errors.title)}>
+                                        <Input id="title" placeholder="Title" value={values.title} onChange={handleChange} />
+                                    </FormItem>
+                                    <FormItem className="input" label="Issue Summary" required validateStatus={this.getValidationStatus(errors.summary)}>
+                                        <TextArea id="summary" placeholder="Description of Problem" value={values.summary} onChange={handleChange} rows={1} />
+                                    </FormItem>
+                                    <FormItem className="input" label="Issue Annotation" validateStatus={this.getValidationStatus(errors.annotation)}>
+                                        <TextArea id="annotation" placeholder="Personal Notes about Problem" value={values.annotation} onChange={handleChange} rows={1} />
+                                    </FormItem>
+                                    <FormItem className="input" label="Issue Type" required validateStatus={this.getValidationStatus(errors.types)}>
+                                        <Select maxTagCount={3}  mode="multiple" defaultValue={['Bug']} className="select" id="types" onChange={(x: any) => setFieldValue("types", x)} value={values.types}>{this.renderDropdowns(painPointList)}</Select>
+                                    </FormItem>
+                                    <FormItem label="Issue Severity" required validateStatus={this.getValidationStatus(errors.priorityLevel)}>
+                                        <div className="wrapper">
+                                            <Icon type="fire" theme="twoTone" twoToneColor="#ff3300" />
+                                            <Slider tipFormatter={this.sliderFormatter} className="slider" id="painPointSeveritySlide" min={0} max={100} onChange={this.slideChange} value={typeof inputValue === 'number' ? inputValue : 0} />
+                                            <Icon type="fire" theme="twoTone" twoToneColor="#ff3300" />
+                                            <Icon type="fire" theme="twoTone" twoToneColor="#ff3300" />
+                                            <Icon type="fire" theme="twoTone" twoToneColor="#ff3300" />
+                                        </div>
+                                    </FormItem>
                                 </div>
-                            </FormItem>
-                        </div>
-                        <div className="customer-components">
-                            <FormItem className="input" label="Company Contact" validateStatus={this.getValidationStatus(errors.companyContact)}>
-                                <Input id="companyContact" placeholder="Company Contact Name" onChange={handleChange} value={values.companyContact} />
-                            </FormItem>
-                            <FormItem className="input" label="Company Name" validateStatus={this.getValidationStatus(errors.companyName)}>
-                                <Input id="companyName" placeholder="Company Name" onChange={handleChange} value={values.companyName} />
-                            </FormItem>
-                            <FormItem className="input" label="Company Location" validateStatus={this.getValidationStatus(errors.companyLocation)}>
-                                <Input id="companyLocation" placeholder="Company Location" onChange={handleChange} value={values.companyLocation} />
-                            </FormItem>
-                            <FormItem className="input" label="Industry Type" validateStatus={this.getValidationStatus(errors.industryType)}>
-                                <Select id="industryType" onChange={(x:any) => setFieldValue("industryType", x)} value={values.industryType}>{this.renderDropdowns(industryList)}</Select>
-                            </FormItem>
-                            <FormItem>
-                                <Button loading={this.props.isSubmitting} onClick={this.props.handleSubmit} className="button" id="submit" htmlType="submit">Submit Problem</Button>
-                            </FormItem>
-                        </div>
-                    </Form>
-                    <div>
-            </div>
+                                <div className="customer-components">
+                                    <FormItem className="input" label="Company Contact" validateStatus={this.getValidationStatus(errors.companyContact)}>
+                                        <Input id="companyContact" placeholder="Company Contact Name" onChange={handleChange} value={values.companyContact} />
+                                    </FormItem>
+                                    <FormItem className="input" label="Company Name" validateStatus={this.getValidationStatus(errors.companyName)}>
+                                        <Input id="companyName" placeholder="Company Name" onChange={handleChange} value={values.companyName} />
+                                    </FormItem>
+                                    <FormItem className="input" label="Company Location" validateStatus={this.getValidationStatus(errors.companyLocation)}>
+                                        <Input id="companyLocation" placeholder="Company Location" onChange={handleChange} value={values.companyLocation} />
+                                    </FormItem>
+                                    <FormItem className="input" label="Industry Type" validateStatus={this.getValidationStatus(errors.industryType)}>
+                                        <Select id="industryType" onChange={(x: any) => setFieldValue("industryType", x)} value={values.industryType}>{this.renderDropdowns(industryList)}</Select>
+                                    </FormItem>
+                                    <FormItem>
+                                        <Button loading={this.props.isSubmitting} onClick={this.props.handleSubmit} className="button" id="submit" htmlType="submit">Submit Problem</Button>
+                                    </FormItem>
+                                </div>
+                            </Form>
+                        </Card>
+                    </Card>
                 </Content>
             </Layout>
         )
@@ -159,7 +160,7 @@ export default withFormik<ICreateFormProps, ICreateFormState>({
         userId: 1
     }),
     validationSchema: yupValidation,
-    handleSubmit: async (values, { setSubmitting, props}) =>  {
+    handleSubmit: async (values, { setSubmitting, props }) => {
         console.log(values);
         console.log(props.painPointHandler);
         const painPointHandler = new PainPointHandler();
